ee47b3b9ebe20330b0f8d8ae64d4f9c14c2a7304
JumperLuko <jumper.luko@gmail.com>
2025-03-16 21:22:44 -0300

    WORK with a FOR!
    DORIME

--- encode-subfolders.bash
+++ encode-subfolders.bash
@@ -10,38 +10,42 @@ SRC_DIR="$1"
 DST_DIR="$2"
 
 # Ensure target directory exists
-# mkdir -p "$DST_DIR"
+mkdir -p "$DST_DIR"
 # echo "$SRC_DIR"
 # echo "$DST_DIR"
 
-# Create folders, separately, because, unknown problems
-find "$SRC_DIR" -type f -name "*.mp4" | while read file; do
-	rel_path=${file#$SRC_DIR}
-	target_path="$DST_DIR$rel_path"
-  target_dir=$(dirname "$target_path")
-	mkdir -p "$target_dir"
-  # echo "$rel_path"
-  # echo "$target_path"
-  # echo "$target_dir"
-  # echo "$file"
-  # exit
-done
+# # Create folders, separately, because, unknown problems
+# find "$SRC_DIR" -type f -name "*.mp4" | while read file; do
+# 	rel_path=${file#$SRC_DIR}
+# 	target_path="$DST_DIR$rel_path"
+#   target_dir=$(dirname "$target_path")
+# 	mkdir -p "$target_dir"
+#   # echo "$rel_path"
+#   # echo "$target_path"
+#   # echo "$target_dir"
+#   # echo "$file"
+#   # exit
+# done
 
 # echo ""
 
-# Use find to scan all the folders and files in the source directory
-find "$SRC_DIR" -type f -name "*.mp4" | while read file; do
-  # Get the relative path of the file
-  rel_path=${file#$SRC_DIR}
-  target_path="$DST_DIR$rel_path"
-  target_dir=$(dirname "$target_path")
-  
-  ffmpeg -n -threads 16 -i "$file" -c:v libsvtav1 -crf 40 -preset 8 -b:v 0 -c:a libvorbis "$target_path.webm"
-  # echo "$file"
-  # echo "$target_path.webm"
-  # echo "$target_dir"
-  # exit
-done
+  # Use find to scan all the folders and files in the source directory
+  for file in "$SRC_DIR"**/*.mp4; do
+    # Get the relative path of the file
+    rel_path=${file#$SRC_DIR}
+    target_path="$DST_DIR$rel_path"
+    target_dir=$(dirname "$target_path")
+    
+    # echo "$file"
+    # echo ""
+    ffmpeg -n -threads 16 -i "$file" -c:v libsvtav1 -crf 63 -preset 13 -b:v 0 -c:a libvorbis "$target_path.webm";
+    # $(ffmpeg -n -threads 16 -i "$file" -c:v libsvtav1 -crf 40 -preset 8 -b:v 0 -c:a libvorbis "$target_path.webm") 2>&1 && sleep 3 || sleep 0.5;
+    # unset rel_path target_path target_dir;
+    # echo "$file"
+    # echo "$target_path.webm"
+    # echo "$target_dir"
+    # exit
+  done
 
 
 
