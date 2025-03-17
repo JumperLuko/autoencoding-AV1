ff11c91b7010492a87bfeb74812918e69c74b54d
JumperLuko <jumper.luko@gmail.com>
2025-03-16 21:30:36 -0300

    Clean code

--- encode-subfolders.bash
+++ encode-subfolders.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# PLEASE, RUN 2 TIMES THIS SCRIPT, I don't know why this bug :(
-# And for external files, please mount in some place
-
 # Please use / in the end of the path
+# And for external files, please mount in some place
 
 # Set the source and destination directories
 SRC_DIR="$1"
@@ -11,80 +9,27 @@ DST_DIR="$2"
 
 # Ensure target directory exists
 mkdir -p "$DST_DIR"
-# echo "$SRC_DIR"
-# echo "$DST_DIR"
 
-# # Create folders, separately, because, unknown problems
-# find "$SRC_DIR" -type f -name "*.mp4" | while read file; do
-# 	rel_path=${file#$SRC_DIR}
-# 	target_path="$DST_DIR$rel_path"
-#   target_dir=$(dirname "$target_path")
-# 	mkdir -p "$target_dir"
-#   # echo "$rel_path"
-#   # echo "$target_path"
-#   # echo "$target_dir"
-#   # echo "$file"
-#   # exit
-# done
+# Create folders
+find "$SRC_DIR" -type f -name "*.mp4" | while read file; do
+	rel_path=${file#$SRC_DIR}
+	target_path="$DST_DIR$rel_path"
+  target_dir=$(dirname "$target_path")
+	mkdir -p "$target_dir"
+done
 
-# echo ""
+# Use all the folders and files in the source directory
+# "for" is needed, because with "read" and "while" has some bug with ffmpeg
+for file in "$SRC_DIR"**/*.mp4; do
+  # Get the relative path of the file
+  rel_path=${file#$SRC_DIR}
+  target_path="$DST_DIR$rel_path"
+  target_dir=$(dirname "$target_path")
 
-  # Use find to scan all the folders and files in the source directory
-  for file in "$SRC_DIR"**/*.mp4; do
-    # Get the relative path of the file
-    rel_path=${file#$SRC_DIR}
-    target_path="$DST_DIR$rel_path"
-    target_dir=$(dirname "$target_path")
-    
-    # echo "$file"
-    # echo ""
-    ffmpeg -n -threads 16 -i "$file" -c:v libsvtav1 -crf 63 -preset 13 -b:v 0 -c:a libvorbis "$target_path.webm";
-    # $(ffmpeg -n -threads 16 -i "$file" -c:v libsvtav1 -crf 40 -preset 8 -b:v 0 -c:a libvorbis "$target_path.webm") 2>&1 && sleep 3 || sleep 0.5;
-    # unset rel_path target_path target_dir;
-    # echo "$file"
-    # echo "$target_path.webm"
-    # echo "$target_dir"
-    # exit
-  done
+  ffmpeg -n -threads 16 -i "$file" -c:v libsvtav1 -crf 40 -preset 8 -b:v 0 -c:a libvorbis "$target_path.webm";
+done
 
-
-
-# # Use find to scan all the folders and files in the source directory
-# find "$SRC_DIR" -type f -name "*.mp4" | while read file; do
-#   # Get the relative path of the file
-#   rel_path=${file#$SRC_DIR/}
-
-#   # Create the destination directory if it doesn't exist
-#   #dst_dir="$DST_DIR/$rel_path"
-#   #dst_dir=${dst_dir%/*}
-#   #mkdir -p "$dst_dir"
-#   target_path="$DST_DIR/$rel_path"
-#   target_dir=$(dirname "$target_path")
-#   #mkdir -p "$target_dir"
-  
-
-#   # Run the ffmpeg command to convert the mp4 file to webm
-#   #ffmpeg -i "$file" -c:v libsvtav1 -crf 50 -preset 13 -b:v 0 -c:a libvorbis "$dst_dir/${file##*/}.webm"
-#   #echo "$file" 
-#   #echo "$dst_dir/${file##*/}.webm"
-  
-#   ffmpeg -n -i "$file" -c:v libsvtav1 -crf 50 -preset 13 -b:v 0 -c:a libvorbis "$target_path.webm"
-#   #my_function () {
-#     #filef=$file
-#     #pathf=$target_path.webm
-    
-#     #echo ffmpeg -i "$file" -c:v libsvtav1 -crf 50 -preset 13 -b:v 0 -c:a libvorbis "$target_path.webm"
-#   #}
-#   #my_function
-#   #echo "Converted $file to $target_path.webm"
-#   #echo "---"
-#   #echo ffmpeg -i "$file" -c:v libsvtav1 -crf 50 -preset 13 -b:v 0 -c:a libvorbis "$target_path.webm"
-# done
-
-
-
-# #!/bin/bash
-
+## Original generated
 # # Set the source and destination directories
 # SRC_DIR=/path/to/source/directory
 # DST_DIR=/path/to/destination/directory
